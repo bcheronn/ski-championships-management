0d64cf16a4933959772ffc27a19647f1a84adbe7
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-10-14 15:23:51 +0200

    Category class created

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -1,6 +1,16 @@
 <?php
 
+namespace Scm\Tests\Unit;
+
+use Scm\Category;
+
 /**
- * Initial test to validate Pest
+ * Initial test to validate Pest and phpunit-watcher
  */
-it('asserts true is true')->assertTrue(true);
+it('should work')->assertTrue(true);
+
+/**
+ * Assert that it is an instance of Category
+ */
+$category = new Category;
+it("shoulb be an instance of Category")->expect($category)->toBeInstanceOf(Category::class);
