e5939e3d4ee804ffc29ac91b391cdb9313286865
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-10-15 11:29:13 +0200

    Category updated (properties added)

--- src/Category.php
+++ src/Category.php
@@ -5,6 +5,10 @@ namespace Scm;
 /**
  * Category
  */
-class Category
+class Category extends Metadata
 {
+    public function create(){}
+    public function update(){}
+    public function delete(){}
+    public function list(){}
 }
