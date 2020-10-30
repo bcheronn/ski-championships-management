3100bf10fb068d1b19cc7d302f04581fe6c98727
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-10-30 15:11:51 +0100

    Router added in the front controller

--- public/index.php
+++ public/index.php
@@ -9,28 +9,34 @@
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 try {
-    // Load the routes from a YAML config file
+    // Initialise the file loader
     $fileLocator = new FileLocator(__DIR__ . "/../config");
     $loader = new YamlFileLoader($fileLocator);
-    $routes = $loader->load("routes.yml");
 
     // Initialise the request from PHP globals and the context from that request
     $request = Request::createFromGlobals();
     $context = new RequestContext();
     $context->fromRequest($request);
 
-    // Matches the requested URL with a route
-    $matcher = new UrlMatcher($routes, $context);
-    $attributes = $matcher->match($request->getPathInfo());
-    dump($attributes);
+    // Initialise the router from the configured routes in YAML
+    $router = new Router(
+        new YamlFileLoader($fileLocator),
+        "routes.yml",
+        [],
+        $context
+    );
+
+    // Find the controller in the set of routes from the URL
+    $controller = $router->match($context->getPathInfo());
+    dump($controller);
 
     // Initialise the response
     $response = new Response("OK");
@@ -38,10 +44,10 @@ try {
     $response = new Response("Not Found", 404);
 } catch (Exception $exception) {
     $response = new Response("An error occurred", 500);
+} finally {
+    // Prepare the response from the request and send it
+    $response->prepare($request);
+    $response->send();
 }
-
-// Prepare the response from the request and send it
-$response->prepare($request);
-$response->send();
 
 dump($GLOBALS);
